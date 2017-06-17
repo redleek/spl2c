5542ed29e53b7545875eb61ed4179afdd27e9403
Connor Blakey <connorblakey96@gmail.com>
2017-06-17 20:18:40 +0100

    Added ability to pass a file to compile via command line

--- spl.c
+++ spl.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+
+extern FILE *yyin;
 int yyparse(void);
 
-int main(void)
+int main(int argc, char **argv)
 {
 #if YYDEBUG == 1
   extern int yydebug;
   yydebug = 1;
 #endif
+
+    --argc; ++argv;
+    if (argc > 0)
+	yyin = fopen(argv[0], "r");
     return(yyparse());
 }
 
